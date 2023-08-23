617a0c2ab63d3b82d7b7944bb504941265017053
ddbranham <101298037+ddbranham@users.noreply.github.com>
2023-08-23 14:28:39 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.ddbranham@gmail.com/" target="_blank">
-          Deveyon Branham{" "}
-        </a>
-        and is{" "}
-        <a href="https://github.com/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.ddbranham@gmail.com/" target="_blank">
+            Deveyon Branham{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/ddbranham/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
