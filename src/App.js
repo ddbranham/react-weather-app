b6f2e62503ce04c0fff5f877985f121a8f339e96
ddbranham <101298037+ddbranham@users.noreply.github.com>
2023-08-23 13:49:14 -0400

    Added Github Repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.ddbranham@gmail.com/" target="_blank">
+          Deveyon Branham{" "}
+        </a>
+        and is{" "}
+        <a href="https://github.com/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
